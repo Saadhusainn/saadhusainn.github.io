b729f1713790aeb5006038e96a1f9c3ca0ad35c4
سعد حسين <130274527+Saadhusainn@users.noreply.github.com>
2026-06-15 08:21:07 +0530

    Update Bukhari file URL to use CDN

--- hadith/hadith-library.js
+++ hadith/hadith-library.js
@@ -12,7 +12,7 @@ const BOOKS_MANIFEST = [
     {
         id: "bukhari",
         // USE CDN URL for fast loading!
-        file: "https://raw.githubusercontent.com/HadithDatabase/data/master/bukhari.json",
+        file: "https://cdn.jsdelivr.net/gh/HadithDatabase/data@master/bukhari.json",
         // Fallback local file
         localFile: "data/bukhari.json",
         name_en: "Ṣaḥīḥ al-Bukhārī",
